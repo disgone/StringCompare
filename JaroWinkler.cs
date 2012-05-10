89394fb39740745b8d7e378ef318022be1768d28
unknown <spsmith@PWDA349.hks.pvt>
2012-05-10 15:53:48 -0500

    Initial import

---
+++ JaroWinkler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace StringCompare
+{
+    public class JaroWinkler
+    {
+        private string matcha;
+        private string matchb;
+
+        public JaroWinkler()
+        {
+            
+        }
+
+        public double GetSimilarity(string a, string b)
+        {
+            if(String.IsNullOrEmpty(a) || String.IsNullOrEmpty(b))
+                return 0;
+
+            double weight = 0;
+            int halflen = Math.Min(a.Length, b.Length) / 2 + 1;
+            string commonA = GetCommonCharacters(a, b, halflen);
+            int common = commonA.Length;
+
+            if (common == 0)
+                return 0;
+
+            string commonB = GetCommonCharacters(b, a, halflen);
+            if (common != commonB.Length)
+                return 0;
+
+            int transpositions = 0;
+            for (int i = 0; i < common; i++)
+            {
+                if (commonA[i] != commonB[i])
+                    transpositions++;
+            }
+
+            transpositions /= 2;
+            weight = common / (3.0 * a.Length) + common / (3.0 * b.Length) + (common - transpositions) / (3.0 * common);
+
+            return weight;
+        }
+
+        public string GetCommonCharacters(string a, string b, int distance)
+        {
+            string commons = "";
+            char[] copy = b.ToCharArray();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char ch = a[i];
+                bool found = false;
+                for (int j = Math.Max(0, i - distance); !found && j < Math.Min(i + distance, b.Length); j++)
+                {
+                    if (copy[j] == ch)
+                    {
+                        found = true;
+                        commons += ch;
+                        copy[j] = '#';
+                    }
+                }
+            }
+
+            return commons;
+        }
+    }
+}
---
+++ Levenshtein.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace StringCompare
+{
+    public static class Levenshtein
+    {
+    	public static int GetDistance(string a, string b)
+    	{
+            if (a == null || b == null)
+            {
+                return -1;
+            }
+
+            int n = a.Length;
+            int m = b.Length;
+
+            if (n == 0)
+                return m;
+            else if (m == 0)
+                return n;
+
+            if (n > m)
+            {
+                string tmp = a;
+                a = b;
+                b = tmp;
+                n = m;
+                m = b.Length;
+            }
+
+            int[] d = new int[n + 1];
+            int[] p = new int[n + 1];
+            int[] _ph; // placeholder
+
+            int i, j;
+            char t_j;
+            int cost;
+
+            for (i = 0; i < n; i++)
+            {
+                p[i] = i;
+            }
+
+            for (j = 1; j <= m; j++)
+            {
+                t_j = b[j - 1];
+                d[0] = j;
+
+                for (i = 1; i <= n; i++)
+                {
+                    cost = a[i - 1] == t_j ? 0 : 1;
+                    d[i] = Math.Min(Math.Min(d[i - 1] + 1, p[i] + 1), p[i - 1] + cost);
+                }
+
+                _ph = p;
+                p = d;
+                d = _ph;
+            }
+
+            return p[n];
+    	}
+    }
+}
